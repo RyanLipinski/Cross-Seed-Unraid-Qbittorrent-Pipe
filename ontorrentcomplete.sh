05532b3a7b1c0cfd818f3ffc866fd2bdadc07db8
Ryan <ryanlipinski1@gmail.com>
2023-09-29 15:38:40 -0600

    Update ontorrentcomplete.sh

--- ontorrentcomplete.sh
+++ ontorrentcomplete.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 "%N"=$1
+
+#Change the path below to the path of your crosswatch.txt file. This needs to be the filepath as QBittorrent sees it (i.e. the QBittorrent container mapping). Do not remove the quotation marks.
 echo $1 >> /scripts/crosswatch/crosswatch.txt
